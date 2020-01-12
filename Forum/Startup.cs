bb9e06bef63458dc0f27185296f0e706ed1e1d12
Georgi Tsonkov <gtsonkov@abv.bg>
2020-01-12 21:09:05 +0100

    Remove unnecessary usings

--- Forum/Startup.cs
+++ Forum/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
@@ -10,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Forum.Data;
 using Forum.Models;
-using Forum.Services;
 
 namespace Forum
 {
